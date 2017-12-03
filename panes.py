1f60cd72fc1e19f79193035760db86a8c9dcb1ff
Eric Woudenberg <eric.woudenberg@jibo.com>
2017-12-02 22:48:53 -0500

    Generate modern Applescript. Refactor so that the heavy lifting is done in Python.

--- panes.py
+++ panes.py
@@ -1,118 +1,61 @@
-#!/usr/bin/python2.7
+#!/usr/bin/env python
 import os
 import sys
 import subprocess
 import tempfile
-from string import Template
 import ConfigParser
-import ast
 import argparse
 
-# iTerm 2 AppleScript scripts adapted from 
-# https://github.com/luismartingil/per.scripts/blob/master/iterm_launcher02.applescript
-# Author: Luis Martin Gil
-# Website: http://www.luismartingil.com/
+# A refactoring of Manbolo's panes.py
+# This configures iTerm2 from a simple configuration file (see example at bottom).
 
 code_template = '''
--- Applescript to launch iterm2 terminals/tabs with configurable:
--- ~ List of commands <cmds>
--- ~ Color <color>
--- ~ Name <name>
--- ~ Transparency <trans>
--- ~ Zoom out <zoomout>
--- ~ Split behavior horizontal(h) or vertical(v) <split> :: (h, v)
---
--- Run from terminal with `osascript` or just ./<<script>>
--- Dont unfocus with the mouse/keyboard while executing. the script.
--- Recomended to go full screen (CMD + Enter) if <zoomout> attributes used.
--- Change myTermWindow and myPane(s) as desired.
---
---
--- Author: Luis Martin Gil http://www.luismartingil.com
--- Year : 2013
-
-tell application "iTerm"
-
-    set myPane to {}
-    $items
-
-    set myTermWindow to {myPane}
-
-    set myterm to (make new terminal)
-
-    tell myterm
-        repeat with n from 1 to count of myTermWindow
-            launch session n
-            repeat with i from 1 to count of (item n of myTermWindow)
-                tell the last session
-                    -- Lets set the properties of the actual tab
-                    set name to name of (item i of (item n of myTermWindow))
-                    -- set background color to color of (item i of (item n of myTermWindow))
-                    -- set transparency to trans of (item i of (item n of myTermWindow))
-                    -- Some commands might require more columns to be readable
-                    repeat zoomout of (item i of (item n of myTermWindow)) times
-                        tell i term application "System Events" to keystroke "-" using command down
-                    end repeat
-                    -- Lets execute the commands for the tab
-                    repeat with cmd in cmds of (item i of (item n of myTermWindow))
-                        write text cmd
-                    end repeat
-                    -- Split the pane in a "D" (vertical) or "d" (horizontal) way
-                    if i is less than (count of (item n of myTermWindow)) then
-                        if "horizontal" is split of (item i of (item n of myTermWindow)) then
-                            set split_str to "D"
-                        else if "vertical" is split of (item i of (item n of myTermWindow)) then
-                            set split_str to "d"
-                        else
-                            error
-                            return
-                        end if
-                        tell i term application "System Events" to keystroke split_str using command down
-                        delay 1
-                    end if
-                end tell
-            end repeat
-        end repeat
-    end tell
+tell application "iTerm2"
+	set myterm to (create window with default profile)
+    %s
 end tell
+return
 '''
 
 
-def get_pane_snippet(cmds=None, name="Default", split=None):
-    """Returns the AppleScript snippet to create an iTerm 2 pane.
-    
-    Keyword arguments:
-    cmds -- the list of bash commands to execute at the start of the shell. (default None)
-    name -- the name of the pane. (default "Default")
-    split -- the format of the next split pane: "horizontal" for horizontal, "vertical" for vertical. (default None)
-    """
-    # Compute the cmds string
-    cmds_str = '","'.join(cmds)
-    cmds_str = '{{"{0}"}}'.format(cmds_str)
+def get_pane_snippet(index, cmds, name, split):
+    txt = '''
+        tell item %d of sessions of current tab of myterm
+            select
+        end tell
+        ''' % index
 
-    pane_snippet = '''
-    set myPane to myPane & {{color:"black", cmds:$cmds, name:"$name", trans:"1.0", zoomout:0, split:"$split"}}
-    '''
-    pane = Template(pane_snippet)
-    pane = pane.substitute(name=name, cmds=cmds_str, split=split)
-    return pane
+    for cmd in cmds:
+        txt += '''
+        tell current session of myterm
+            write text "%s"
+        end tell
+        ''' % cmd
+
+    txt += '''
+    tell current session of myterm
+        set name to "%s"
+    end tell
+    ''' % name
+
+    txt += '''
+    tell current session of myterm
+        split %s with default profile
+    end tell
+    ''' % ("vertically" if split.startswith('v') else "horizontally")
+
+    return txt
 
 
 def get_apple_script(config):
-    """Returns the AppleScript script to configure iTerm 2.
-
-    Keyword arguments:
-    config -- ConfigParser instance to transform into AppleScript.
-    """
-    body = Template(code_template)
+    index = 0
     panes_str = ''
-
-    # For each section of the config file, construct an AppleScript
-    # command.
+    # Construct the Applescript for each section of the config file.
     for section in config.sections():
+        index += 1
         name = section
-        cmds = None
-        split = None
+        cmds = []
+        split = 'horizontal'
 
         if config.has_option(section, 'cmds'):
             cmds = config.get(section, 'cmds')
@@ -121,22 +64,48 @@ def get_apple_script(config):
         if config.has_option(section, 'split'):
             split = config.get(section, 'split')
 
-        panes_str += get_pane_snippet(cmds, name, split)
+        panes_str += get_pane_snippet(index, cmds, name, split)
 
     # Inject the AppleScript command in the code template
-    body = body.substitute(items=panes_str)
-    return body
+    return code_template % panes_str
 
 
 def launch_apple_script(conf_file):
-    """Launch the AppleScript script to configure iTerm 2.
+    config = ConfigParser.ConfigParser()
+    config.read(conf_file)
+    body = get_apple_script(config)
 
-    Keyword arguments:
-    conf_file -- path of the configuration file.
-    
+    # Create temporary Apple script file and launch it.
+    temp = tempfile.NamedTemporaryFile(delete=False)
+    temp.write(body)
+    temp.close()
+    cmd = "osascript {0}".format(temp.name)
+    subprocess.call(cmd, shell=True)
+    os.unlink(temp.name)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'config', nargs='?', help='name of the iTerm 2 config in ~/.panesrc directory (e.g. config=django will uses ~/.panesrc/djanog.conf)', default='default')
+    args = parser.parse_args()
+
+    config_path = os.path.expanduser('~/.panesrc/{}.conf'.format(args.config))
+
+    if not os.path.exists(config_path):
+        print("No config file at {0}".format(config_path))
+        sys.exit(0)
+
+    launch_apple_script(config_path)
+
+
+if __name__ == '__main__':
+    main()
+
+"""
     Example of a configuration file:
-    
-    [Projet]
+
+    [Project]
 
     cmds =
         export DJANGO_SETTINGS_MODULE=example.settings.local
@@ -154,37 +123,11 @@ def launch_apple_script(conf_file):
         source ~/venvs/python2.7/bin/activate
 
     split = horizontal
-    
+
     [Apache logs]
 
     cmds =
         ssh dev.example.com
         tail -f /example.com/access.log
 
-    """
-    config = ConfigParser.ConfigParser()
-    config.read(conf_file)
-    body = get_apple_script(config)
-
-    # Create temporary Apple script file and launch it.
-    temp = tempfile.NamedTemporaryFile(delete=False)
-    temp.write(body)
-    temp.close()
-    cmd = "osascript {0}".format(temp.name)
-    subprocess.call(cmd, shell=True)
-    os.unlink(temp.name)
-
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('config', nargs='?', help='name of the iTerm 2 config in ~/.panesrc directory (config=django will uses ~/.panesrc/djanog.conf)', default='default')
-    args = parser.parse_args()
-
-    config_path = os.path.expanduser('~/.panesrc/{}.conf'.format(args.config))
-
-    if not os.path.exists(config_path):
-        print("No config file at {0}".format(config_path))
-        sys.exit(0)
-
-    launch_apple_script(config_path)
+"""
